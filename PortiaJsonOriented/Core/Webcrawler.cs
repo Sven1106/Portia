a3b610589a5512083bdd171990216eac47dac73d
Svend Aage Roperos Nielsen <shaddyd-sarn@live.dk>
2019-12-09 14:25:11 +0100

    Refraction

--- PortiaJsonOriented/Core/Webcrawler.cs
+++ PortiaJsonOriented/Core/Webcrawler.cs
@@ -22,10 +22,11 @@ namespace PortiaJsonOriented
         private static string dequeuedUrlsFile = "dequeuedUrls.txt";
         private static string allQueuedUrlsFile = "allQueuedUrls.txt";
         private static string allSuccesfullUrlsFile = "allSuccesfullUrls.txt";
-        private static BlockingCollection<Uri> queue = new BlockingCollection<Uri>();
+        private static BlockingCollection<Uri> uriQueue = new BlockingCollection<Uri>();
         private static BlockingCollection<Uri> visitedUrls = new BlockingCollection<Uri>();
         private static ConcurrentDictionary<string, JArray> tasks = new ConcurrentDictionary<string, JArray>();
         private static ConcurrentDictionary<int, string> IdMessagePairs = new ConcurrentDictionary<int, string>();
+        private static ConcurrentDictionary<Uri, string> UriHtmlPairs = new ConcurrentDictionary<Uri, string>();
         private static IList<string> disallowedStrings = new List<string>() { };
         private static Uri rootUri;
         private static int itemSuccessfullyCrawledCount = 0;
@@ -48,13 +49,50 @@ namespace PortiaJsonOriented
             disallowedStrings = request.DisallowedStrings;
 
             rootUri = new Uri(request.StartUrl);
-            queue.TryAdd(rootUri);
+            uriQueue.TryAdd(rootUri);
 
             foreach (var item in dataForRequest)
             {
                 tasks.TryAdd(item.TaskName, new JArray());
             }
 
+            await Task.Run(async () => await InitCrawler(4));
+
+            await Task.Run(async () => await InitValueExtractor(2));
+
+
+            Core.Dtos.Response response = new Core.Dtos.Response
+            {
+                ProjectName = request.ProjectName,
+                StartUrl = request.StartUrl,
+                Data = tasks
+            };
+            return response;
+        }
+        public static async Task InitValueExtractor(int threadCount)
+        {
+            IList<Task> Workers = new List<Task>();
+            for (int i = 0; i < threadCount; i++)
+            {
+                Task task = Task.Run(() =>
+                {
+                    ValueExtractorWorker();
+                });
+                Workers.Add(task);
+            }
+            await Task.WhenAll(Workers);
+
+        }
+
+        public static void ValueExtractorWorker()
+        {
+            foreach (var uriHtml in UriHtmlPairs)
+            {
+                ExtractAndMapValues(uriHtml.Key, uriHtml.Value);
+            }
+        }
+        public static async Task InitCrawler(int threadCount)
+        {
             await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
             //Enabled headless option
             var args = new string[] {
@@ -66,100 +104,128 @@ namespace PortiaJsonOriented
                 "--disable-permissions-api", "--disable-background-networking", "--disable-3d-apis",
                 "--disable-bundled-ppapi-flash"
             };
+
             var launchOptions = new LaunchOptions { Headless = false, Args = args, IgnoreHTTPSErrors = true };
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
             using (var browser = await Puppeteer.LaunchAsync(launchOptions))
             {
-                int threadCount = 4;// Environment.ProcessorCount;// Use max 4 threads to crawl
-                IList<Task> taskList = new List<Task>();
+                IList<Task> crawlWorkers = new List<Task>();
                 for (int i = 0; i < threadCount; i++)
                 {
-                    int workerId = i;
                     Task task = Task.Run(async () =>
                     {
-                        await Worker(workerId, browser);
+                        await CrawlerWorker(browser);
                     });
-                    taskList.Add(task);
+                    crawlWorkers.Add(task);
                 }
-                await Task.WhenAll(taskList);
+                await Task.WhenAll(crawlWorkers);
             }
-            stopwatch.Stop();
-            Console.WriteLine(stopwatch.Elapsed);
-
-            Core.Dtos.Response response = new Core.Dtos.Response
-            {
-                ProjectName = request.ProjectName,
-                StartUrl = request.StartUrl,
-                Data = tasks
-            };
-            return response;
         }
-
-        public static async Task Worker(int workerId, Browser browser)
+        public static async Task CrawlerWorker(Browser browser)
         {
-            await Task.Run(async () =>
+            Console.WriteLine("Worker {0} is starting.", Task.CurrentId);
+            foreach (var workItem in uriQueue.GetConsumingEnumerable())
             {
-                Console.WriteLine("Worker {0} is starting.", workerId);
-                foreach (var workItem in queue.GetConsumingEnumerable())
+                Uri currentUrl = workItem;
+                //Console.WriteLine("Worker {0} is processing uri: {1}", workerId, currentUrl);
+                File.AppendAllText(dequeuedUrlsFile, currentUrl.ToString() + Environment.NewLine);
+                visitedUrls.TryAdd(currentUrl);
+                Interlocked.Increment(ref crawledUrlsCount);
+
+                string html = await GetHtmlAsync(browser, currentUrl);
+                HtmlDocument htmlDoc = new HtmlDocument();
+                htmlDoc.LoadHtml(html);
+                AddNewUrlsToQueue(disallowedStrings, rootUri, uriQueue, visitedUrls, htmlDoc);
+                UriHtmlPairs.TryAdd(currentUrl, html);
+                // Console.Write("\rUrls in queue: {0} - Urls visited: {1} - Items successfully crawled: {2}", queue.Count, crawledUrlsCount, itemSuccessfullyCrawledCount);
+                if (uriQueue.Count <= 0)
                 {
-                    Uri currentUrl = workItem;
-                    //Console.WriteLine("Worker {0} is processing uri: {1}", workerId, currentUrl);
-                    File.AppendAllText(dequeuedUrlsFile, currentUrl.ToString() + Environment.NewLine);
-                    visitedUrls.TryAdd(currentUrl);
-                    Interlocked.Increment(ref crawledUrlsCount);
-
-                    string html = await GetWithPuppeteer(browser, currentUrl);
-                    HtmlDocument htmlDoc = new HtmlDocument();
-                    htmlDoc.LoadHtml(html);
-                    AddNewUrlsToQueue(disallowedStrings, rootUri, ref queue, visitedUrls, htmlDoc);
-                    HtmlNode documentNode = htmlDoc.DocumentNode;
-                    foreach (DataForRequest task in dataForRequest)
-                    {
-                        JObject taskObject = new JObject();
-                        foreach (NodeAttribute item in task.Items)
-                        {
-                            JToken value = GetValueForJTokenRecursive(item, documentNode);
-                            if (value.ToString() == "")
-                            {
-                                continue;
-                            }
-                            taskObject.Add(item.Name, value);
-                            Metadata metadata = new Metadata(currentUrl.ToString(), DateTime.UtcNow);
-                            taskObject.Add("metadata", JObject.FromObject(metadata, new JsonSerializer()
-                            {
-                                ContractResolver = new CamelCasePropertyNamesContractResolver()
-                            }));
-
-                        }
-                        if (taskObject.HasValues == false)
-                        {
-                            continue;
-                        }
-                        tasks[task.TaskName].Add(taskObject);
-                        File.AppendAllText(allSuccesfullUrlsFile, currentUrl.ToString() + Environment.NewLine);
-                        Interlocked.Increment(ref itemSuccessfullyCrawledCount);
-                    }
-                    Console.Write("\rUrls in queue: {0} - Urls visited: {1} - Items successfully crawled: {2}", queue.Count, crawledUrlsCount, itemSuccessfullyCrawledCount);
-
-                    if (queue.Count <= 0)
-                    {
-                        queue.CompleteAdding(); // Add this to the HtmlWorker
-                    }
+                    uriQueue.CompleteAdding(); // Add this to the HtmlWorker
                 }
-                Console.WriteLine("\rWorker {0} is stopping.", workerId);
-            });
+            }
+            Console.WriteLine("\rWorker {0} is stopping.", Task.CurrentId);
 
         }
-
-        public static async Task WorkerMessage()
+        private static async Task<string> GetHtmlAsync(Browser browser, Uri uri)
         {
-            while (true)
+            string content = "";
+            using (var page = await browser.NewPageAsync())
             {
-                foreach (var item in IdMessagePairs)
+                await page.SetRequestInterceptionAsync(true);
+
+                page.Request += Page_Request;
+                await page.GoToAsync(uri.ToString());
+                content = await page.GetContentAsync();
+            }
+            return content;
+        }
+        private static async void Page_Request(object sender, RequestEventArgs e)
+        {
+            try
+            {
+                switch (e.Request.ResourceType)
                 {
-                    Console.WriteLine(item.Value);
+
+                    case ResourceType.Media:
+                    case ResourceType.StyleSheet:
+                    case ResourceType.Image:
+                    case ResourceType.Unknown:
+                    case ResourceType.Font:
+                    case ResourceType.TextTrack:
+                    case ResourceType.Xhr:
+                    case ResourceType.Fetch:
+                    case ResourceType.EventSource:
+                    case ResourceType.WebSocket:
+                    case ResourceType.Manifest:
+                    case ResourceType.Ping:
+                    case ResourceType.Other:
+                        await e.Request.AbortAsync();
+                        break;
+                    case ResourceType.Document:
+                    case ResourceType.Script:
+                    default:
+                        await e.Request.ContinueAsync();
+                        break;
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error => {ex.Message}");
+                await e.Request.ContinueAsync();
+            }
+        }
+
+
+
+        private static void ExtractAndMapValues(Uri currentUrl, string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            HtmlNode documentNode = htmlDoc.DocumentNode;
+            foreach (DataForRequest task in dataForRequest)
+            {
+                JObject taskObject = new JObject();
+                foreach (NodeAttribute item in task.Items)
+                {
+                    JToken value = GetValueForJTokenRecursive(item, documentNode);
+                    if (value.ToString() == "")
+                    {
+                        continue;
+                    }
+                    taskObject.Add(item.Name, value);
+                    Metadata metadata = new Metadata(currentUrl.ToString(), DateTime.UtcNow);
+                    taskObject.Add("metadata", JObject.FromObject(metadata, new JsonSerializer()
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver()
+                    }));
+
+                }
+                if (taskObject.HasValues == false)
+                {
+                    continue;
+                }
+                tasks[task.TaskName].Add(taskObject);
+                // File.AppendAllText(allSuccesfullUrlsFile, currentUrl.ToString() + Environment.NewLine);
+                Interlocked.Increment(ref itemSuccessfullyCrawledCount);
             }
         }
         private static JToken GetValueForJTokenRecursive(NodeAttribute node, HtmlNode htmlNode)
@@ -237,7 +303,7 @@ namespace PortiaJsonOriented
             }
             return jToken;
         }
-        private static void AddNewUrlsToQueue(IList<string> disallowedStrings, Uri rootUri, ref BlockingCollection<Uri> queue, BlockingCollection<Uri> visitedUrls, HtmlDocument htmlDoc)
+        private static void AddNewUrlsToQueue(IList<string> disallowedStrings, Uri rootUri, BlockingCollection<Uri> queue, BlockingCollection<Uri> visitedUrls, HtmlDocument htmlDoc)
         {
             var aTags = htmlDoc.DocumentNode.SelectNodes("//a[@href]");
             if (aTags != null)
@@ -274,53 +340,5 @@ namespace PortiaJsonOriented
             return false;
         }
 
-        private static async Task<string> GetWithPuppeteer(Browser browser, Uri uri)
-        {
-            string content = "";
-            using (var page = await browser.NewPageAsync())
-            {
-                await page.SetRequestInterceptionAsync(true);
-
-                page.Request += Page_Request;
-                await page.GoToAsync(uri.ToString());
-                content = await page.GetContentAsync();
-            }
-            return content;
-        }
-        private static async void Page_Request(object sender, RequestEventArgs e)
-        {
-            try
-            {
-                switch (e.Request.ResourceType)
-                {
-
-                    case ResourceType.Media:
-                    case ResourceType.StyleSheet:
-                    case ResourceType.Image:
-                    case ResourceType.Unknown:
-                    case ResourceType.Font:
-                    case ResourceType.Script:
-                    case ResourceType.TextTrack:
-                    case ResourceType.Xhr:
-                    case ResourceType.Fetch:
-                    case ResourceType.EventSource:
-                    case ResourceType.WebSocket:
-                    case ResourceType.Manifest:
-                    case ResourceType.Ping:
-                    case ResourceType.Other:
-                        await e.Request.AbortAsync();
-                        break;
-                    case ResourceType.Document:
-                    default:
-                        await e.Request.ContinueAsync();
-                        break;
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error => {ex.Message}");
-                await e.Request.ContinueAsync();
-            }
-        }
     }
 }
